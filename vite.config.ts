6eee0e100b78d16e3272275e88e1e32a84dc7296
Wesley Ferreira <107949749+Wesleyfbarretos@users.noreply.github.com>
2023-06-21 21:18:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/portifolio-web',
+  plugins: [react()]
 });
